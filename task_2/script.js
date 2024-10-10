5e828e159139001e204a49bd873280bfae848b8e
Sameliuk <86837651+Sameliuk@users.noreply.github.com>
2024-10-10 17:48:05 +0300

    Update script.js

--- task_2/script.js
+++ task_2/script.js
@@ -1,56 +1,32 @@
-const sendBtn1 = document.getElementById('send-btn-1');
-const sendBtn2 = document.getElementById('send-btn-2');
-
-const inputMessage1 = document.getElementById('message-input-1');
-const inputMessage2 = document.getElementById('message-input-2');
-
 const chatBox = document.getElementById('chat-box');
 
-const btn1Click = () => {
-    const messageText1 = inputMessage1.value.trim();
+const massageButton = (inputElement, userClass, userName) => {
+    const messageText = inputElement.value.trim();
 
-    if (messageText1 !== "") {
-        const messageElement1 = document.createElement('div');
-        messageElement1.classList.add('message-1');
+    if (messageText !== "") {
+        const messageElement = document.createElement('div');
+        messageElement.classList.add(userClass);
 
-        const userTitle1 = document.createElement('strong');
-        userTitle1.textContent = 'USER 1';
+        const userTitle = document.createElement('strong');
+        userTitle.textContent = userName;
 
-        const messageTextElement1 = document.createElement('p');
-        messageTextElement1.textContent = messageText1;
+        const messageTextElement = document.createElement('p');
+        messageTextElement.textContent = messageText;
 
-        messageElement1.appendChild(userTitle1);
-        messageElement1.appendChild(messageTextElement1);
+        messageElement.appendChild(userTitle);
+        messageElement.appendChild(messageTextElement);
 
-        chatBox.appendChild(messageElement1);
-        chatBox.scrollTop = document.getElementById('chat-box').scrollHeight;
+        chatBox.appendChild(messageElement);
+        chatBox.scrollTop = chatBox.scrollHeight;
 
-        inputMessage1.value = '';
+        inputElement.value = '';
     }
 };
 
-const btn2Click = () => {
-    const messageText2 = inputMessage2.value.trim();
+document.getElementById('send-btn-1').addEventListener('click', () => {
+    massageButton(document.getElementById('message-input-1'), 'message-1', 'USER 1');
+});
 
-    if (messageText2 !== "") {
-        const messageElement2 = document.createElement('div');
-        messageElement2.classList.add('message-2');
-
-        const userTitle2 = document.createElement('strong');
-        userTitle2.textContent = 'USER 2';
-
-        const messageTextElement2 = document.createElement('p');
-        messageTextElement2.textContent = messageText2;
-
-        messageElement2.appendChild(userTitle2);
-        messageElement2.appendChild(messageTextElement2);
-
-        chatBox.appendChild(messageElement2);
-        chatBox.scrollTop = document.getElementById('chat-box').scrollHeight;
-
-        inputMessage2.value = '';
-    }
-};
-
-sendBtn1.addEventListener('click', btn1Click);
-sendBtn2.addEventListener('click', btn2Click);
+document.getElementById('send-btn-2').addEventListener('click', () => {
+    massageButton(document.getElementById('message-input-2'), 'message-2', 'USER 2');
+});
